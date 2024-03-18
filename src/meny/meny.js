e0317476de78381b9ec5974f93a7e05754fd1d11
Mankey <hampusviskan12@gmail.com>
2024-03-18 12:56:08 +0100

    Fixade allt till samma sida

--- src/meny/meny.js
+++ src/meny/meny.js
@@ -1,1 +1,26 @@
-document.getElementById('rätter');
+const params = new URLSearchParams(window.location.search)
+if(params.has('restaurang')) {
+    const restaurang = params.get('restaurang');
+    if(restaurang != 'at' && restaurang != 'hemkop' && restaurang != 'NSU') window.location.href = '../home.html';
+} else {
+    //window.location.href = '../home.html';
+}
+
+//Retrieve foods
+let time = new Date();
+
+const hour = time.getHours()
+console.log(hour);
+
+var obj;
+
+fetch('../assets/json/mat.json')
+  .then(res => res.json())
+  .then(data => {
+    obj = data;
+   })
+  .then(() => {
+    console.log(obj);
+   });
+
+console.log(obj);
